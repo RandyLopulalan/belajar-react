9dbd41e4480f0c96d2ff9d5eae3ddea467f1e37c
Lopulalan Randy Yonathan <randylopulalan07@gmail.com>
2022-05-14 15:07:22 +0800

    Form & Validation

--- src/Materi/Form/Form/form.js
+++ src/Materi/Form/Form/form.js
@@ -1,26 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import Validation from "../Validation/validation";
 import "./form.css";
 
 const Form = () => {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [valid, setValid] = useState(false);
+  const [check, setCheck] = useState({
+    lowercaseCheck : false,
+    uppercaseCheck: false,
+    numberCheck: false,
+    charactereCheck: false,
+    lengthCheck: false
+  });
+  const [data, setData] = useState([]);
+  // const validPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+  const validPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!validPassword.test(password)) return setValid(true)
+    result(user, password)
+    setUser("")
+    setPassword("") 
+  };
+
+  const handleCheck = (e) => {
+    const {value} = e.target
+    const lowercaseCheck = /[a-z]/.test(value)
+    const uppercaseCheck = /[A-Z]/.test(value)
+    const numberCheck = /[0-9]/.test(value)
+    const charactereCheck = /[!@#$%^&*]/.test(value)
+    const lengthCheck = value.length >= 8
+    setCheck({
+      lowercaseCheck,uppercaseCheck,numberCheck,charactereCheck,lengthCheck
+    })
+  }
+  const result = (user, password) => {
+    const newResult = { id: 1, user: user, password: password };
+    const listResult = [...data,newResult]
+    setData(listResult);
+  };
+
   return (
-    <form className="form">
-      <div className="form_user">
-        <label>
-          UserName
-          <input type="text" name="name" />
-        </label>
+    <div className="form_container">
+      {/* ===================== Form ================== */}
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <h3>Login</h3>
+          <div className="form_user">
+            <input
+              type="text"
+              name="name"
+              value={user}
+              onChange={(e) => setUser((prev) => (prev = e.target.value))}
+              autoFocus
+              required
+            />
+            <label>UserName</label>
+          </div>
+          <div className="form_user">
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword((prev) => (prev = e.target.value))}
+              onKeyUp={handleCheck}
+              autoComplete="new-password current-passwword"
+              autoFocus
+              required
+            />
+            <label>Password</label>
+            {valid && (<Validation 
+            lowercaseValid={check.lowercaseCheck ? "valid" : "invalid"}
+            uppercaseValid={check.uppercaseCheck ? "valid" : "invalid"}
+            numberValid={check.numberCheck ? "valid" : "invalid"}
+            characterValid={check.charactereCheck ? "valid" : "invalid"}
+            lengthValid={check.lengthCheck ? "valid" : "invalid"}
+            />)}
+          </div>
+          <button className="form_btn_submit" type='submit'>Login</button>
+        </form>
       </div>
-      <div className="form_user">
-        <label>
-          Password
-          <input
-            type="password"
-            name="password"
-            autoComplete="new-password current-passwword"
-          />
-        </label>
+      {/* ===================== Form RESULT ================== */}
+      <div className="result">
+        {data.map(data => {
+          return (
+            <li key={data.id}>
+              <h2>User List</h2>
+              <h4>{`UserName : ${data.user}`}</h4>
+              <h4>{`Password : ${data.password}`}</h4>
+            </li>
+          );
+        })}
       </div>
-    </form>
+    </div>
   );
 };
 
--- src/Materi/Form/Validation/validation.js
+++ src/Materi/Form/Validation/validation.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const Validation = () => {
+const Validation = ({
+  lowercaseValid,
+  uppercaseValid,
+  numberValid,
+  characterValid,
+  lengthValid,
+}) => {
   return (
-    <div>Validation</div>
-  )
-}
+    <div className="validation">
+      <h3>Password :</h3>
+      <p className={lowercaseValid}>must contain lowercase letter</p>
+      <p className={uppercaseValid}>must contain uppercase letter</p>
+      <p className={numberValid}>must contain number</p>
+      <p className={characterValid}>must contain special character</p>
+      <p className={lengthValid}>must be eight characters or longer</p>
+    </div>
+  );
+};
 
-export default Validation
+export default Validation;
